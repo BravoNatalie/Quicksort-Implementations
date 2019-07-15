360136cffc09bca0bf27784a49263611fa0b7e38
BravoNatalie <natalie.bravo@outlook.com>
2019-07-15 17:30:42 -0300

    results methods 3 and 4

--- results.py
+++ results.py
@@ -3,14 +3,14 @@ import random
 import quicksort
 
 
-def runningTime():
+def runningTime(results, method):
     instances = ['instances_level_1_100.txt', 'instances_level_1_1000.txt',
                  'instances_level_1_10000.txt', 'instances_level_2_100.txt',
                  'instances_level_2_1000.txt', 'instances_level_2_10000.txt',
                  'instances_level_3_100.txt', 'instances_level_3_1000.txt',
                  'instances_level_3_10000.txt']
 
-    results = open('results.txt', 'w')
+    results = open(results, 'w+')
 
     for instance in instances:
         with open(instance, 'r') as file:
@@ -18,7 +18,7 @@ def runningTime():
             for line in file:
                 vec = line.split(' ')
                 start = time.time()
-                quicksort.quicksort(vec)
+                quicksort.quicksort(vec, method)
                 end = time.time()
                 mean += (end - start)
             mean /= 10
@@ -27,4 +27,5 @@ def runningTime():
             results.write(instance + ' mean time: ' + str(time_ms) + '\n')
 
 
+runningTime('results_method_4.txt', 4)
 
